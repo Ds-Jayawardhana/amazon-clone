8e822bc2c525389f66b14f230d5a6055e6da2e58
Disandu Sanhida Jayawardhane <dissaaa12345@gmail.com>
2024-09-13 18:49:36 +0530

    cart Modification

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -705,7 +705,8 @@ products.forEach((product)=>{
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-cart"
+              data-product-id="${product.id}">
                 Add to Cart
             </button>
             </div>`
@@ -716,3 +717,35 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').
 innerHTML=productsHTML;
+
+document.querySelectorAll('.js-add-cart')
+  .forEach((button)=>{
+    button.addEventListener('click',()=>{
+      const productId=button.dataset.
+      productId;
+
+      let matchingItem;
+
+      cart.forEach((item)=>{
+        if(productId===item.productId){
+          matchingItem=item;
+        }
+      });
+
+      if(matchingItem){
+        matchingItem.quantity+=1
+      }else{
+        cart.push({
+          productId:productId,
+          quantity:1
+        });
+        
+
+      }
+
+      console.log(cart);
+      
+    });
+
+  });
+
