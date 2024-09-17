be8e5f599b6287753cd947e70bd9e087b5c6c108
Disandu Sanhida Jayawardhane <w2083055@westminster.ac.uk>
2024-09-18 00:11:04 +0530

    Save Data and Generate HTML For Payment Summary

--- Scripts/Checkout/orderSummary.js
+++ Scripts/Checkout/orderSummary.js
@@ -1,7 +1,7 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 
  
@@ -15,26 +15,14 @@ export function renderOrderSummary(){
 
       const productId=item.productId;
 
-      let matchingProduct;
+      const matchingProduct=getProduct(productId);
 
-      products.forEach((product)=>{
-
-          if(product.id===productId){
-              matchingProduct=product;
-          }
-
-      });
+     
 
       
     const deliveryOptionId=item.deliveryOptionId;
 
-      let deliveryOption;
-
-      deliveryOptions.forEach((option)=>{
-        if(option.id === deliveryOptionId){
-          deliveryOption = option;
-        }
-      });
+  const deliveryOption=getDeliveryOption(deliveryOptionId);
 
     const today=dayjs();
     const deliveryDate=today.add(
@@ -166,5 +154,7 @@ export function renderOrderSummary(){
 
         });
       });
+      
 }
+
 
