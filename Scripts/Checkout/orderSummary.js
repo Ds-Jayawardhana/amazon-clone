cae33022c952464ddc15850defd133d259b10ba0
Disandu Sanhida Jayawardhane <w2083055@westminster.ac.uk>
2024-09-18 00:24:57 +0530

    Make payment Summary Interactive

--- Scripts/Checkout/orderSummary.js
+++ Scripts/Checkout/orderSummary.js
@@ -2,7 +2,8 @@ import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
 import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
  
 let cartSummaryHTML='';
@@ -141,6 +142,8 @@ export function renderOrderSummary(){
 
           container.remove();
 
+          renderPaymentSummary();
+
       });
       
     });
@@ -151,7 +154,7 @@ export function renderOrderSummary(){
           const {productId,deliveryOptionId}=element.dataset;
           updateDeliveryOption(productId,deliveryOptionId);
           
-
+          renderPaymentSummary();
         });
       });
       
