43048190b5e743b4f07b9a02e05e249a1e2ec966
Disandu Sanhida Jayawardhane <w2083055@westminster.ac.uk>
2024-09-18 13:48:31 +0530

    Create Automated tests for Format Currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,29 @@
+import {formatCurrency} from '../Scripts/utils/money.js';
+
+
+
+console.log('Test suite:FormatCurrency');
+
+console.log('convert cents into dollars');
+
+if (formatCurrency(2095)==='20.95'){
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('works with 0')
+
+if(formatCurrency(0)=='0.00'){
+    console.log('passed');
+} else {
+    console.log('failed')
+}
+
+console.log('rounds upto nearest cent')
+
+if(formatCurrency(2000.5)==='20.01'){
+    console.log('passed');
+}else{
+    console.log('failed')
+}
